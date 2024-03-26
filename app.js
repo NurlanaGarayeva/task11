19858744e0203dfc64675de64326f43ac8eb7207
NurlanaGarayeva <nurqarayeva075@gmail.com>
2024-03-25 23:16:45 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+// const str = 'html java css vs ';
+
+// console.log(`${str} ${str.length}`);
+
+
+
+// let text = "daxil etdiyimiz 2ci soz";
+// console.log(text.startsWith(""));
+
+
+
+// let text = "daxil etdiyimiz 2ci soz";
+// console.log(text.endsWith(""));
+
+
+
+let name=prompt("daxil etdiyimiz simvol nece herfden ibaretdi")
+
+let uzunluq=name.length;
+console.log(uzunluq);
+
+let soz=prompt("daxil etdiyimiz 2ci soz")
+
+
+console.log(name.includes(uzunluq))
+
+
+console.log(text.startsWith("uzunluq"));
+
+console.log(text.endsWith("uzunluq"));
+
+
+console.log(text.indexOf("uzunluq"));
+
+// function input(name){
+//     if(name===HELLO toupperCase()){
+//         return true;
+//     }
+  
+// }
+Function isuppercase(name){
+    return name === name.toupperCase();
+    console.log(isuppercase(name));
+}
+
+
